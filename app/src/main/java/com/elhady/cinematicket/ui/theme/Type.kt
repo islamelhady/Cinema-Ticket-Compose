18c6e5b95aa0fda9f5fe067582ad4761b252472d
islam elhady <i.elhady44@gmail.com>
2025-06-22 15:52:02 +0300

    add move name and move duration components

--- app/src/main/java/com/elhady/cinematicket/ui/theme/Type.kt
+++ app/src/main/java/com/elhady/cinematicket/ui/theme/Type.kt
@@ -1,13 +1,23 @@
 package com.elhady.cinematicket.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 30.sp,
+        letterSpacing = 0.15.sp,
+        color = Black,
+        textAlign = TextAlign.Center
+    ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
