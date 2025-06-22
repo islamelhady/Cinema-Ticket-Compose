fe8dbc47834856f99e67ee3e1f9d90ab99917bec
islam elhady <i.elhady44@gmail.com>
2025-06-22 15:19:06 +0300

    rename openSans to Capital letters

--- app/src/main/java/com/elhady/cinematicket/ui/theme/Font.kt
+++ app/src/main/java/com/elhady/cinematicket/ui/theme/Font.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import com.elhady.cinematicket.R
 
-val openSans = FontFamily(
+val OpenSans = FontFamily(
     Font(R.font.open_sans_regular, weight = FontWeight.Normal),
     Font(R.font.open_sans_medium, weight = FontWeight.Medium),
     Font(R.font.open_sans_semi_bold, weight = FontWeight.SemiBold),
