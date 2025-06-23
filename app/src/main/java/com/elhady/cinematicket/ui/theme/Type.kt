4ac372ad397b290df3bb78397bb513bab689f0ae
islam elhady <i.elhady44@gmail.com>
2025-06-23 23:44:26 +0300

    DELETE: DELETE DetailsHeader.kt

--- app/src/main/java/com/elhady/cinematicket/ui/theme/Type.kt
+++ app/src/main/java/com/elhady/cinematicket/ui/theme/Type.kt
@@ -1,17 +1,21 @@
 package com.elhady.cinematicket.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     titleLarge = TextStyle(
         fontFamily = OpenSans,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontSize = 30.sp,
         letterSpacing = 0.15.sp,
+        color = Color.Black,
+        textAlign = TextAlign.Center,
     ),
     titleMedium = TextStyle(
         fontFamily = OpenSans,
@@ -33,6 +37,9 @@ val Typography = Typography(
     bodyMedium = TextStyle(
         fontFamily = OpenSans,
         fontWeight = FontWeight.Normal,
+        color = Color.Black,
+        textAlign = TextAlign.Center,
+        letterSpacing = 0.25.sp,
         fontSize = 14.sp,
     ),
     bodySmall = TextStyle(
