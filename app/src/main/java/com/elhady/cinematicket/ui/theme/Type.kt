47f2c30b6cd20275e499b48ce6435a65d5c023a1
islam elhady <i.elhady44@gmail.com>
2025-06-23 15:17:24 +0300

    feat: update typography with custom fonts and add component previews

--- app/src/main/java/com/elhady/cinematicket/ui/theme/Type.kt
+++ app/src/main/java/com/elhady/cinematicket/ui/theme/Type.kt
@@ -1,11 +1,8 @@
 package com.elhady.cinematicket.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
@@ -15,30 +12,39 @@ val Typography = Typography(
         fontWeight = FontWeight.Medium,
         fontSize = 30.sp,
         letterSpacing = 0.15.sp,
-        color = Black,
-        textAlign = TextAlign.Center
+    ),
+    titleMedium = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
     ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = OpenSans,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
