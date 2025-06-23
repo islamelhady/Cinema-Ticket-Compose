c1ad40db974f9ad6333e4bf2962bbdcd445022be
islam elhady <i.elhady44@gmail.com>
2025-06-23 23:19:20 +0300

    refactor: enhance DetailsHeader UI component

--- app/src/main/java/com/elhady/cinematicket/ui/screen/details/components/DetailsHeader.kt
+++ app/src/main/java/com/elhady/cinematicket/ui/screen/details/components/DetailsHeader.kt
@@ -2,23 +2,18 @@ package com.elhady.cinematicket.ui.screen.details.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -26,14 +21,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.elhady.cinematicket.R
+import com.elhady.cinematicket.ui.components.CircularIcon
 import com.elhady.cinematicket.ui.components.MovieDuration
 import com.elhady.cinematicket.ui.theme.Grey
 import com.elhady.cinematicket.ui.theme.Orange
 
 @Composable
 fun DetailsHeader(
-    modifier: Modifier = Modifier,
-    onCloseClick: () -> Unit = {}
+    modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier
@@ -41,55 +36,41 @@ fun DetailsHeader(
             .height(400.dp)
     ) {
         Image(
-            painter = painterResource(id = R.drawable.morbius),
+            painter = painterResource(id = R.drawable.fantastic_beasts_banner),
             contentDescription = null,
             modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.FillWidth
         )
-
-        IconButton(
-            onClick = onCloseClick,
+        Column(
             modifier = Modifier
-                .padding(16.dp)
-                .align(Alignment.TopStart)
-                .clip(CircleShape)
-                .background(Color.White.copy(alpha = 0.3f))
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(112.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Icon(imageVector = Icons.Default.Close, contentDescription = null, tint = Color.White)
-        }
-
-        MovieDuration(
-            modifier = Modifier
-                .padding(16.dp)
-                .background(Grey.copy(alpha = 0.6f), shape = CircleShape)
-                .align(Alignment.TopEnd),
-            movieDuration = "2h 23m",
-            textColor = White,
-            iconTint = Grey,
-            textSize = 14.sp,
-            iconSize = 24.dp
-        )
-
-        Box(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .size(64.dp)
-                .clip(CircleShape)
-                .background(Orange),
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                imageVector = Icons.Default.PlayArrow,
-                contentDescription = null,
-                tint = Color.White,
-                modifier = Modifier.size(32.dp)
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                CircularIcon()
+                MovieDuration(
+                    modifier = Modifier
+                        .background(Grey.copy(alpha = 0.6f), shape = CircleShape)
+                        .padding(all = 4.dp),
+                    textColor = White,
+                    iconTint = Grey,
+                    textSize = 14.sp,
+                    iconSize = 24.dp
+                )
+            }
+            CircularIcon(icon = painterResource(id = R.drawable.play), backgroundColor = Orange)
         }
     }
 }
 
 @Composable
 @Preview
-fun DetailsHeaderPreview(){
+fun DetailsHeaderPreview() {
     DetailsHeader()
 }
